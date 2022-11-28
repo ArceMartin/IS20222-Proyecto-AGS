f4d55008eb05230eff1e074472063a3ec1561623
Martin Arce <13899640+ArceMartin@users.noreply.github.com>
2022-11-28 11:26:22 -0800

    Modificaciones carpeta Martin

--- MARTIN/AGS-Backend/app.js
+++ MARTIN/AGS-Backend/app.js
@@ -1,57 +1,62 @@
 //Importación de metodos 
-const express = require("express");
-const app = express();
-const fs=require("fs");
-const https=require("https");
-process.env.port=3001;
-const Alumnos=require("./Rutas/Alumnos");
-const Cursos=require("./Rutas/Cursos");
-const Tareas=require("./Rutas/Tareas");
-const Entregas=require("./Rutas/ListaEntregas");
-const ObtenerE=require("./Rutas/ObtenerEntrega");
+const express    = require("express");
+const app        = express();
+const fs         = require("fs");
+const https      = require("https");
+const cors       = require('cors');
+const path       = require('path');
+process.env.port = 3001;
+
+const Alumnos    = require("./Rutas/Alumnos");
+const Cursos     = require("./Rutas/Cursos");
+const Tareas     = require("./Rutas/Tareas");
+const Entregas   = require("./Rutas/ListaEntregas");
+const ObtenerE   = require("./Rutas/ObtenerEntrega");
+
 app.use(express.json());
-const cors=require('cors');
 app.use(cors());
 app.use(express.json());
 
-
 const fsp = fs.promises;
-const path = require('path');
-//const process = require('process');
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly',
-'https://www.googleapis.com/auth/classroom.courses',
-'https://www.googleapis.com/auth/classroom.rosters',
-'https://www.googleapis.com/auth/classroom.rosters.readonly',
-'https://www.googleapis.com/auth/classroom.profile.emails',
-'https://www.googleapis.com/auth/classroom.profile.photos',
-'https://www.googleapis.com/auth/classroom.coursework.students.readonly',
-'https://www.googleapis.com/auth/classroom.coursework.me.readonly',
-'https://www.googleapis.com/auth/classroom.coursework.students',
-'https://www.googleapis.com/auth/classroom.coursework.me',
-'https://www.googleapis.com/auth/classroom.coursework.me',
-'https://www.googleapis.com/auth/classroom.coursework.me.readonly',
-'https://www.googleapis.com/auth/classroom.coursework.students',
-'https://www.googleapis.com/auth/classroom.coursework.students.readonly',
-'https://www.googleapis.com/auth/classroom.student-submissions.me.readonly',
-'https://www.googleapis.com/auth/classroom.student-submissions.students.readonly',
-'https://www.googleapis.com/auth/classroom.coursework.me',
-'https://www.googleapis.com/auth/classroom.coursework.me.readonly',
-'https://www.googleapis.com/auth/classroom.coursework.students',
-'https://www.googleapis.com/auth/classroom.coursework.students.readonly',
-'https://www.googleapis.com/auth/classroom.student-submissions.me.readonly',
-'https://www.googleapis.com/auth/classroom.student-submissions.students.readonly',];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
+// Si modifica estos ámbitos, elimine token.json.
+const googleapis = 'https://www.googleapis.com/auth/classroom.';
+const SCOPES = [
+  googleapis + 'courses',
+  googleapis + 'courses.readonly',
+  googleapis + 'coursework.me',
+  googleapis + 'coursework.me',
+  googleapis + 'coursework.me',
+  googleapis + 'coursework.me.readonly',
+  googleapis + 'coursework.me.readonly',
+  googleapis + 'coursework.me.readonly',
+  googleapis + 'coursework.students',
+  googleapis + 'coursework.students',
+  googleapis + 'coursework.students',
+  googleapis + 'coursework.students.readonly',
+  googleapis + 'coursework.students.readonly',
+  googleapis + 'coursework.students.readonly',
+  googleapis + 'profile.emails',
+  googleapis + 'profile.photos',
+  googleapis + 'rosters',
+  googleapis + 'rosters.readonly',
+  googleapis + 'student-submissions.me.readonly',
+  googleapis + 'student-submissions.me.readonly',
+  googleapis + 'student-submissions.students.readonly',
+  googleapis + 'student-submissions.students.readonly',
+];
+
+// El archivo token.json almacena los tokens de acceso y actualización del usuario, y se crea 
+// automáticamente cuando el flujo de autorización se completa por primera vez.
+// const TOKEN_PATH = path.join(process.cwd(), 'token_martin.json');
+// const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials_martin.json');
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
 /**
- * Reads previously authorized credentials from the save file.
+ * Lee las credenciales previamente autorizadas del archivo guardado.
  *
  * @return {Promise<OAuth2Client|null>}
  */
@@ -66,7 +71,7 @@ async function loadSavedCredentialsIfExist() {
 }
 
 /**
- * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ * Serializa las credenciales a un archivo compatible con GoogleAUth.fromJSON.
  *
  * @param {OAuth2Client} client
  * @return {Promise<void>}
@@ -83,31 +88,34 @@ async function saveCredentials(client) {
   });
   await fsp.writeFile(TOKEN_PATH, payload);
 }
+
+/**
+ * Carga o solicitud o autorización para llamar a las API.
+ *
+ */
 async function authorize() {
-    let client = await loadSavedCredentialsIfExist();
-    if (client) {
-      return client;
-    }
-    client = await authenticate({
-      scopes: SCOPES,
-      keyfilePath: CREDENTIALS_PATH,
-    });
-    if (client.credentials) {
-      await saveCredentials(client);
-    }
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
     return client;
   }
-
-
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
 
 
 //Declaracion de certificado
 const llavePrivada = fs.readFileSync("private.key");
-const certificado = fs.readFileSync("certificate.crt");
+const certificado  = fs.readFileSync("certificate.crt");
 const credenciales = {
- key: llavePrivada,
- cert: certificado,
- passphrase: "contraseña" //passwd de la llave privada usado en la creación del certificado
+  key: llavePrivada,
+  cert: certificado,
+  passphrase: "contraseña" //passwd de la llave privada usado en la creación del certificado
 };
 
 const httpsServer = https.createServer(credenciales, app);
@@ -131,7 +139,7 @@ app.use("/Entregas", Entregas);
 app.use("/ObtenerE", ObtenerE);
 
 
-httpsServer.listen(process.env.port, () => {
+httpsServer.listen(process.env.port, "0.0.0.0", () => {
     console.log('Servidor https escuchando por el puerto:', process.env.port);
    }).on('error', err => {
     console.log('Error al inciar el servidor:', err);
